1df6f469b2e1f0407c1f3425e7ffa590a2067413
ridiiatov <r.idiyatov@iilab.ru>
2024-03-03 22:13:11 +0300

    feat(products): feat products module

---
+++ src/products/products.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productService: ProductsService) {}
+  @Get()
+  getAll() {
+    return this.productService.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.productService.getById(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Cache-Control', 'none')
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productService.create(createProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return 'Remove' + id;
+  }
+
+  @Put(':id')
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
+    return `Update ${id}`;
+  }
+}
